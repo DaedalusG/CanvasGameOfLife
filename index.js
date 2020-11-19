7785a668545b660376c4aec5cbfb39666c85f4d9
warrgiff <warrenbruceg@gmail.com>
2020-11-19 00:28:12 -0800

    working found the bug, it was a col where a row should have been

--- index.js
+++ index.js
@@ -35,39 +35,8 @@ function makeLifeBox(grid) {
 //make next interation based of rules of conways game of life
 
 function nextGen(grid) {
-    // const nextGen = grid.map(arr => [...arr]);
-
-    // for (let col = 0; col < grid.length; col++) {
-    //     for (let row = 0; row < grid[col].length; row++) {
-    //         const cell = grid[col][row];
-    //         let numNeighbours = 0;
-
-    //         for (let i = -1; i < 2; i++) {
-    //             for (let j = -1; j < 2; j++) {
-    //                 if (i === 0 && j === 0) {
-    //                     continue;
-    //                 }
-    //                 const x_cell = col + i;
-    //                 const y_cell = col + j;
-
-    //                 if (x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
-    //                     const currentNeighbour = grid[col + i][row + j]
-    //                     numNeighbours += currentNeighbour
-    //                 }
-    //             }
-    //         }
-    //         //rules
-    //         if (cell === 1 && numNeighbours < 2) {
-    //             nextGen[col][row] = 0
-    //         } else if (cell === 1 && numNeighbours > 3) {
-    //             nextGen[col][row] = 0
-    //         } else if (cell === 0 && numNeighbours === 3) {
-    //             nextGen[col][row] = 1
-    //         }
-    //     }
-    // }
-    // return nextGen
     const nextGen = grid.map(arr => [...arr]);
+
     for (let col = 0; col < grid.length; col++) {
         for (let row = 0; row < grid[col].length; row++) {
             const cell = grid[col][row];
